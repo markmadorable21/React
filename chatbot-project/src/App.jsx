6f129c3fb97e2862fe2e0bc59e2543f18ea783fb
Mark Madorable <markazjudaya@gmail.com>
2026-06-17 15:37:06 +0800

    proper chatbot project vite react set up

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { ChatInput } from './components/ChatInput';
+import ChatMessages from './components/ChatMessages';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [chatMessages, setChatMessages] = useState([
+    {
+      message: 'hello chatbot',
+      sender: 'user',
+      id: 'id1',
+    },
+    {
+      message: 'Hello! How can I help you?',
+      sender: 'robot',
+      id: 'id2',
+    },
+    {
+      message: 'can you get me todays date?',
+      sender: 'user',
+      id: 'id3',
+    },
+    {
+      message: 'Today is September 27',
+      sender: 'robot',
+      id: 'id4',
+    },
+  ]);
+  // const [chatMessages, setChatMessages] = array;
+  // const chatMessages = array[0];
+  // const setChatMessages = array[1];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
   );
 }
 
