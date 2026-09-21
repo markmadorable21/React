e93cf14b257c62fd6bc510b91d432548501c5320
Mark Madorable <markazjudaya@gmail.com>
2026-09-22 00:57:16 +0800

    created log-in form

--- login-form/src/App.jsx
+++ login-form/src/App.jsx
@@ -1,33 +1,11 @@
-import { useState } from 'react';
+import { LoginForm } from './components/LoginForm';
 import './App.css';
 
 function App() {
-  const [showPassword, setShowPassword] = useState(false);
-
-  function doThisWhenButtonIsClicked() {
-    console.log('Clicked');
-    setShowPassword(!showPassword);
-  }
   return (
     <>
-      <h2>Hello, welcome to my portfolio</h2>
-      <div>
-        <input placeholder="Email" className="email-input" />
-      </div>
-      <div>
-        <input
-          placeholder="Password"
-          className="password-input"
-          type={showPassword ? 'text' : 'password'}
-        />
-        <button className="show-button" onClick={doThisWhenButtonIsClicked}>
-          {showPassword ? 'Hide' : 'Show'}
-        </button>
-      </div>
-      <div>
-        <button className="login-button">Login</button>
-        <button className="signup-button">Sign up</button>
-      </div>
+      <p className="title">Hello, welcome to my website</p>
+      <LoginForm />
     </>
   );
 }
