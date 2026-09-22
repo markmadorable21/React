fc6cfb777fa07605d1fe32d6ed1d65caedacc393
Mark Madorable <markazjudaya@gmail.com>
2026-09-22 09:01:23 +0800

    added orders page htmll, css and routing

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router';
 import { useState } from 'react'
 import {HomePage} from './pages/HomePage.jsx'
 import {CheckoutPage} from './pages/CheckoutPage.jsx'
+import {OrdersPage} from './pages/OrdersPage.jsx'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   )
 }
